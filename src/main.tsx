893568d9120e1f5fe928103aa8c657f17f6aa970
Kravchenko-Sergey <sergey.ose.pyatigorsk@gmail.com>
2023-06-25 18:11:30 +0300

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import './styles/index.scss'
 import { createRoot } from 'react-dom/client'
 
 import { App } from './App.tsx'
-import './index.css'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
